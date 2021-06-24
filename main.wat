d8914033c7fcd488867f6433840f1def8e42565d
Komodo <noreply@komodo.ar>
2021-06-23 22:45:45 -0300

    feat: draw pixels to canvas

--- main.wat
+++ main.wat
@@ -1,7 +1,11 @@
 (module
-	(func $add (export "add") (param $a i32) (param $b i32) (result i32)
-		local.get $a
-		local.get $b
-		i32.add
+	(import "js" "memory" (memory 1))
+	(func (export "fill") (param $size i32)
+		(local $i i32)
+		(loop $loop
+			(i32.store (local.get $i) (i32.const 0xFFFFFFFF))
+			(local.set $i (i32.add (local.get $i) (i32.const 8)))
+			(br_if $loop (i32.lt_u (local.get $i) (local.get $size)))
+		)
 	)
 )
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-WebAssembly.instantiateStreaming(fetch("main.wasm")).then(wasm => {
-	console.log("2 + 2 = " + wasm.instance.exports.add(2, 2));
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d", {
+	alpha: false,
+	// TODO: Unsure what this option does
+	// desynchronized: false
+});
+
+// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
+const pixelsSize = canvas.width * canvas.height * 4;
+// WebAssembly allocates in 4kb pages
+const memory = new WebAssembly.Memory({ initial: Math.ceil(pixelsSize / 65536) });
+const pixels = new Uint8Array(memory.buffer, 0, pixelsSize);
+
+const image = context.createImageData(canvas.width, canvas.height);
+
+WebAssembly.instantiateStreaming(fetch("main.wasm"), { "js": { memory } }).then(wasm => {
+	wasm.instance.exports.fill(pixelsSize);
+	image.data.set(pixels);
+	context.putImageData(image, 0, 0);
 });
