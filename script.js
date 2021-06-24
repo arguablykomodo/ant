dffad8a103b16f736202c18c54b9b9ca9e6a4f81
Komodo <noreply@komodo.ar>
2021-06-24 11:43:50 -0300

    refactor: keep code dry

--- script.js
+++ script.js
@@ -11,37 +11,39 @@ const context = canvas.getContext("2d", {
 
 const image = context.createImageData(canvas.width, canvas.height);
 
-const dataSize = canvas.width * canvas.height;
-// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
-const pixelsSize = canvas.width * canvas.height * 4;
-const palleteSize = (2 ** 8) * 4;
+const layout = new Map([
+	["data", canvas.width * canvas.height],
+	// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
+	["pixels", canvas.width * canvas.height * 4],
+	["pallete", (2 ** 8) * 4],
+]);
 
 // WebAssembly allocates in 4kb pages
+const memorySize = Array.from(layout.values()).reduce((acc, mem) => acc + mem);
 const memory = new WebAssembly.Memory({
-	initial: Math.ceil((dataSize + pixelsSize + palleteSize) / 65536),
+	initial: Math.ceil(memorySize / 65536),
 });
 
-const data = new Uint8Array(memory.buffer, 0, dataSize);
-const pixels = new Uint8Array(memory.buffer, dataSize, pixelsSize);
-const pallete = new Uint8Array(memory.buffer, dataSize + pixelsSize, palleteSize);
+const buffers = {};
+const imports = { memory };
+[...layout].reduce((acc, [name, size]) => {
+	buffers[name] = new Uint8Array(memory.buffer, acc, size);
+	if (acc > 0) imports[name] = new WebAssembly.Global({ value: "i32" }, acc);
+	return acc + size;
+}, 0);
 
 // cyan to magenta gradient
+const palleteSize = layout.get("pallete");
 for (let i = 0; i < palleteSize; i += 4) {
-	pallete[i] = (i / palleteSize) * 256;
-	pallete[i + 1] = 255 - (i / palleteSize) * 256;
-	pallete[i + 2] = 255;
-	pallete[i + 3] = 255;
+	buffers.pallete[i] = (i / palleteSize) * 256;
+	buffers.pallete[i + 1] = 255 - (i / palleteSize) * 256;
+	buffers.pallete[i + 2] = 255;
+	buffers.pallete[i + 3] = 255;
 }
 
-WebAssembly.instantiateStreaming(fetch("main.wasm"), {
-	"js": {
-		pixels: new WebAssembly.Global({ value: "i32" }, dataSize),
-		pallete: new WebAssembly.Global({ value: "i32" }, dataSize + pixelsSize),
-		memory,
-	}
-}).then(wasm => {
-	wasm.instance.exports.fill(dataSize);
-	wasm.instance.exports.draw(dataSize, dataSize + pixelsSize);
-	image.data.set(pixels);
+WebAssembly.instantiateStreaming(fetch("main.wasm"), { "js": imports }).then(wasm => {
+	wasm.instance.exports.fill();
+	wasm.instance.exports.draw();
+	image.data.set(buffers.pixels);
 	context.putImageData(image, 0, 0);
 });
