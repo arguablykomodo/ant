ea2a9d52e20f704348126533dd333b8b4f091204
Komodo <noreply@komodo.ar>
2021-06-24 11:15:12 -0300

    feat: draw from pallete

--- main.wat
+++ main.wat
@@ -1,11 +1,55 @@
 (module
 	(import "js" "memory" (memory 1))
-	(func (export "fill") (param $size i32)
+	(import "js" "pixels" (global $pixels i32))
+	(import "js" "pallete" (global $pallete i32))
+
+	(func $pixel_i (param $i i32) (result i32)
+		(i32.add
+			(i32.mul
+				(local.get $i)
+				(i32.const 4))
+			(global.get $pixels)))
+
+	(func $pallete_i (param $i i32) (result i32)
+		(i32.add
+			(i32.mul
+				(local.get $i)
+				(i32.const 4))
+			(global.get $pallete)))
+
+	(func $fill (export "fill")
+		(local $i i32)
+		(local $acc i32)
+		(loop $loop
+			(i32.store
+				(local.get $i)
+				(local.get $acc))
+			(local.set $i
+				(i32.add
+					(local.get $i)
+					(i32.const 1)))
+			(local.set $acc
+				(i32.rem_u
+					(i32.add
+						(local.get $acc)
+						(i32.const 1))
+					(i32.const 256)))
+			(br_if $loop
+				(i32.lt_u
+					(local.get $i)
+					(global.get $pixels)))))
+
+	(func $draw (export "draw")
 		(local $i i32)
 		(loop $loop
-			(i32.store (local.get $i) (i32.const 0xFFFFFFFF))
-			(local.set $i (i32.add (local.get $i) (i32.const 8)))
-			(br_if $loop (i32.lt_u (local.get $i) (local.get $size)))
-		)
-	)
-)
+			(i32.store
+				(call $pixel_i (local.get $i))
+				(i32.load (call $pallete_i (i32.load8_u (local.get $i)))))
+			(local.set $i
+				(i32.add
+					(local.get $i)
+					(i32.const 1)))
+			(br_if $loop
+				(i32.lt_u
+					(local.get $i)
+					(global.get $pixels))))))
--- script.js
+++ script.js
@@ -9,16 +9,39 @@ const context = canvas.getContext("2d", {
 	// desynchronized: false
 });
 
-// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
-const pixelsSize = canvas.width * canvas.height * 4;
-// WebAssembly allocates in 4kb pages
-const memory = new WebAssembly.Memory({ initial: Math.ceil(pixelsSize / 65536) });
-const pixels = new Uint8Array(memory.buffer, 0, pixelsSize);
-
 const image = context.createImageData(canvas.width, canvas.height);
 
-WebAssembly.instantiateStreaming(fetch("main.wasm"), { "js": { memory } }).then(wasm => {
-	wasm.instance.exports.fill(pixelsSize);
+const dataSize = canvas.width * canvas.height;
+// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
+const pixelsSize = canvas.width * canvas.height * 4;
+const palleteSize = (2 ** 8) * 4;
+
+// WebAssembly allocates in 4kb pages
+const memory = new WebAssembly.Memory({
+	initial: Math.ceil((dataSize + pixelsSize + palleteSize) / 65536),
+});
+
+const data = new Uint8Array(memory.buffer, 0, dataSize);
+const pixels = new Uint8Array(memory.buffer, dataSize, pixelsSize);
+const pallete = new Uint8Array(memory.buffer, dataSize + pixelsSize, palleteSize);
+
+// cyan to magenta gradient
+for (let i = 0; i < palleteSize; i += 4) {
+	pallete[i] = (i / palleteSize) * 256;
+	pallete[i + 1] = 255 - (i / palleteSize) * 256;
+	pallete[i + 2] = 255;
+	pallete[i + 3] = 255;
+}
+
+WebAssembly.instantiateStreaming(fetch("main.wasm"), {
+	"js": {
+		pixels: new WebAssembly.Global({ value: "i32" }, dataSize),
+		pallete: new WebAssembly.Global({ value: "i32" }, dataSize + pixelsSize),
+		memory,
+	}
+}).then(wasm => {
+	wasm.instance.exports.fill(dataSize);
+	wasm.instance.exports.draw(dataSize, dataSize + pixelsSize);
 	image.data.set(pixels);
 	context.putImageData(image, 0, 0);
 });
