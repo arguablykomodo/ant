625c3c5bdf33bc01ee2f55c8e6550374d54d536a
Komodo <noreply@komodo.ar>
2021-06-25 23:12:02 -0300

    refactor: move wasm glue to separate file

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-import { randomPallete } from "./pallete.js"
+import { buildWasm } from "./wasm.js";
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const context = canvas.getContext("2d", {
-	alpha: false,
-	// TODO: Unsure what this option does
-	// desynchronized: false
-});
-
-const image = context.createImageData(canvas.width, canvas.height);
+/** @type {HTMLInputElement} */
+const ruleInput = document.getElementById("rule");
+/** @type {HTMLDivElement} */
+const ruleContainer = document.getElementById("ruleContainer");
+/** @type {HTMLAnchorElement} */
+const link = document.getElementById("link");
 
 const rules = [
 	"LR",
@@ -30,58 +26,34 @@ const rules = [
 	"RRLLLRLLLLL",
 	"RRLRRRLLLLLL",
 	"RLLRRRLRRRRR",
-	"RLRLRRRRRRRL"
+	"RLRLRRRRRRRL",
 ];
-const ruleText = rules[Math.floor(Math.random() * rules.length)];
-document.getElementById("rule").value = ruleText
-const rule = ruleText.split("").map(char => char === "R" ? 1 : 3);
 
-const layout = new Map([
-	["data", canvas.width * canvas.height],
-	// RGBA8. Sadly there seems to be no way to get rid of the alpha channel
-	["pixels", canvas.width * canvas.height * 4],
-	["pallete", (2 ** 8) * 4],
+const rule = rules[Math.floor(Math.random() * rules.length)];
+ruleInput.value = rule;
 
-	["rule", rule.length],
-]);
-
-// WebAssembly allocates in 4kb pages
-const memorySize = Array.from(layout.values()).reduce((acc, mem) => acc + mem);
-const memory = new WebAssembly.Memory({
-	initial: Math.ceil(memorySize / 65536),
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const context = canvas.getContext("2d", {
+	alpha: false,
+	// TODO: Unsure what this option does
+	// desynchronized: false
 });
 
-const buffers = {};
-const imports = { memory };
-[...layout].reduce((acc, [name, size]) => {
-	buffers[name] = new Uint8Array(memory.buffer, acc, size);
-	if (acc > 0) imports[name] = new WebAssembly.Global({ value: "i32" }, acc);
-	imports[name + "_size"] = new WebAssembly.Global({ value: "i32" }, size);
-	return acc + size;
-}, 0);
+const image = context.createImageData(canvas.width, canvas.height);
 
-for (let i = 0; i < rule.length; i++) {
-	buffers.rule[i] = rule[i];
-}
-
-imports.width = new WebAssembly.Global({ value: "i32" }, canvas.width);
-imports.position = new WebAssembly.Global({ value: "i32", mutable: true }, 0);
-imports.direction = new WebAssembly.Global({ value: "i32", mutable: true }, 0);
-
-randomPallete(buffers.pallete, buffers.rule.length);
-
-WebAssembly.instantiateStreaming(fetch("main.wasm"), { "js": imports }).then(wasm => {
+buildWasm(canvas.width, canvas.height, rule).then(({ update, pixels }) => {
 	let steps = 1;
-	function update() {
-		const result = wasm.instance.exports.update(Math.floor(steps));
-		image.data.set(buffers.pixels);
+	function render() {
+		const shouldContinue = update(Math.floor(steps)) === 0;
+		image.data.set(pixels);
 		context.putImageData(image, 0, 0);
 		steps = Math.min(steps * 1.01, 1000000); // Max 1 million steps per frame
-		if (result === 0) requestAnimationFrame(update);
+		if (shouldContinue) requestAnimationFrame(render);
 		else {
-			document.getElementById("ruleContainer").className = "visible";
-			document.getElementById("link").className = "visible";
+			ruleContainer.className = "visible";
+			link.className = "visible";
 		}
 	}
-	update();
+	requestAnimationFrame(render);
 });
---
+++ wasm.js
@@ -0,0 +1,59 @@
+import { randomPallete } from "./pallete.js";
+
+/**
+ * @param {number} value
+ * @param {boolean} [mutable=false]
+ */
+function global(value, mutable = false) {
+	return new WebAssembly.Global({ value: "i32", mutable }, value);
+}
+
+/**
+ * @param {number} width
+ * @param {number} height
+ * @param {string} rule
+ */
+export async function buildWasm(width, height, rule) {
+	const dataSize = width * height;
+
+	const pixelsStart = dataSize;
+	const pixelsSize = dataSize * 4;
+
+	const palleteStart = pixelsStart + pixelsSize;
+	const palleteSize = (2 ** 8) * 4;
+
+	const ruleStart = palleteStart + palleteSize;
+	const ruleSize = rule.length;
+
+	// WebAssembly allocates in 4kb pages
+	const memory = new WebAssembly.Memory({
+		initial: Math.ceil((ruleStart + ruleSize) / 65536),
+	});
+
+	const imports = {
+		memory,
+		data_size: global(dataSize),
+		pallete: global(palleteStart),
+		rule: global(ruleStart),
+		rule_size: global(ruleSize),
+		width: global(width),
+		position: global(0, true),
+		direction: global(0, true),
+	};
+
+	const pixelsBuffer = new Uint8Array(memory.buffer, pixelsStart, pixelsSize);
+	const palleteBuffer = new Uint8Array(memory.buffer, palleteStart, palleteSize);
+	const ruleBuffer = new Uint8Array(memory.buffer, ruleStart, ruleSize);
+
+	randomPallete(palleteBuffer, ruleSize);
+
+	for (let i = 0; i < ruleSize; i++) {
+		ruleBuffer[i] = rule[i] === "R" ? 1 : 3;
+	}
+
+	const wasm = await WebAssembly.instantiateStreaming(fetch("main.wasm"), { js: imports });
+	return {
+		update: wasm.instance.exports.update,
+		pixels: pixelsBuffer,
+	};
+}
